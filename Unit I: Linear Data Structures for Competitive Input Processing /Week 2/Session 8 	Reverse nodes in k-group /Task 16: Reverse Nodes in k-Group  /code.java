afe1e5f2e984e33b9aca301c004c03adc9eea34e
Kushal Vinayak Maddi <maddikushalvinayak@gmail.com>
2026-09-24 09:50:57 +0530

    Implement reverseKGroup for reversing nodes in k-group

---
+++ Unit I: Linear Data Structures for Competitive Input Processing /Week 2/Session 8 	Reverse nodes in k-group /Task 16: Reverse Nodes in k-Group  /code.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || k == 1) return head;
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode prevGroup = dummy;
+
+        while (true) {
+            // Check if there are at least k nodes remaining
+            ListNode kth = getKthNode(prevGroup, k);
+            if (kth == null) break;
+
+            ListNode nextGroup = kth.next;
+            
+            // Reverse the current k nodes
+            ListNode prev = kth.next;
+            ListNode curr = prevGroup.next;
+
+            while (curr != nextGroup) {
+                ListNode temp = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = temp;
+            }
+
+            // Connect with the previous part of the list
+            ListNode tmp = prevGroup.next;
+            prevGroup.next = kth;
+            prevGroup = tmp;
+        }
+
+        return dummy.next;
+    }
+
+    private ListNode getKthNode(ListNode curr, int k) {
+        while (curr != null && k > 0) {
+            curr = curr.next;
+            k--;
+        }
+        return curr;
+    }
+}
